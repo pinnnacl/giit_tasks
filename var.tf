718e33d4510cdf6b61bd8d5b9448114d35e97ad3
pinnnacl <nandusathyan0@gmail.com>
2025-02-21 16:14:41 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,7 +2,7 @@
 variable "subscription_id" {
   description = "Azure subscription ID"
   type        = string
-  default     = "5f926371-d8ba-4687-934a-a14019e51b51" 
+  default     = "******-****-****-****-*********" 
 }
 
 variable "location" {
@@ -21,5 +21,5 @@ variable "admin_password" {
   description = "Admin password for VMs"
   type        = string
   sensitive   = true
-  default     = "P@ssw0rd1234!" # Replace with a strong password
+  default     = "P@ssw0rd1234!"
 }
